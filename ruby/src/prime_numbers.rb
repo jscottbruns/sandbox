f59c20ac8eb9340e42cfaa441fced1e896a8b077
Jeff Bruns <jscottbruns@gmail.com>
2018-09-06 15:46:10 -0400

    prime_numbers.rb

--- ruby/src/prime_numbers.rb
+++ ruby/src/prime_numbers.rb
@@ -1,7 +1,21 @@
+require "benchmark"
+
+$perms
+
 class PrimeNum
   def self.prime?(n)
     m = n/2
     (2..m).each do |i|
+      $perms += 1
+      return false if n % i == 0
+    end
+    true
+  end
+
+  def self.primeroot?(n)
+    m = Math.sqrt(n).to_i
+    (2..m).each do |i|
+      $perms += 1
       return false if n % i == 0
     end
     true
@@ -9,10 +23,31 @@ class PrimeNum
 end
 
 c = ARGV[0].to_i
-primes = []
 
-(2..c).each do |i|
-  primes << i if PrimeNum.prime?(i)
+print "Calculating all primes up to #{c}..."
+
+time = Benchmark.measure do
+  $perms = 0
+  primes = []
+  (2..c).each do |i|
+    primes << i if PrimeNum.prime?(i)
+  end
+  puts "Total primes: #{primes.count}"
 end
 
-puts primes.join(', ')
+puts "Total permutations for method 1: #{$perms}"
+puts "Runtime for method 1: #{time}\n"
+
+time = Benchmark.measure do
+  $perms = 0
+  primes = []
+  (2..c).each do |i|
+    primes << i if PrimeNum.primeroot?(i)
+  end
+  puts "Total primes: #{primes.count}"
+end
+
+puts "Total permutations for method 2: #{$perms}"
+puts "Runtime for method 2: #{time}"
+
+
